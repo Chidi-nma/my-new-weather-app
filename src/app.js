6b9276f2fd7d54cf1ef364e6e615d2cbc6eec749
Chidi <ikebujochidi@gmail.com>
2024-12-28 16:31:47 -0500

    Updated the apiKey in the js file.

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "61350a1ad6529158aboa62df0c4fb6t9";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
